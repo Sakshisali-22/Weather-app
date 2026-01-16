22ac8860efe13d52415cb365b699a856c3936d8d
Sakshisali-22 <sakshisali76@gmail.com>
2026-01-17 02:24:49 +0530

    Initial commit - Weather App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Myapp from './Components/Myapp';
+import Weather from './Components/Weather';
+
+function App() {
+  return (
+    <>
+     
+      <Myapp/>
+    </>
+  );
+}
+
+export default App;
